24d7f86796e69b2e69403a00d4a8cddef15a1a4a
dineshladi <ladidinesh@gmail.com>
2019-12-07 16:02:29 +0530

    Day 2 Solutions added

---
+++ day02/day02.py
@@ -0,0 +1,34 @@
+
+def do_the_operation(lst, noun, verb):
+    data = lst.copy()
+    # Resote gravity assit program
+    data[1] = noun
+    data[2] = verb
+
+    for idx in range(0, len(raw), 4):
+        if (data[idx] == 1):
+            data[data[idx+3]] = data[data[idx+1]] + data[data[idx+2]]
+        elif (data[idx] == 2):
+            data[data[idx+3]] = data[data[idx+1]] * data[data[idx+2]]
+        elif (data[idx] == 99):
+            break
+        else:
+            continue
+    return(data[0])
+
+
+with open('raw.txt') as file:
+    raw = file.read().split(",")
+    raw = list(map(int, raw))
+
+    print(do_the_operation(raw, 12, 2))
+
+    # 2nd part
+
+    for noun in range(0, 99):
+        for ver in range(0, 99):
+            result = do_the_operation(raw, noun, ver)
+            #print(noun, ver)
+            if(result == 19690720):
+                print((100 * noun) + ver)
+                break
